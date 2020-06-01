ebc0eed92f650a9b4cefb4b4a12682b8a34d27f7
lucy991016 <1106059217@qq.com>
2020-06-01 20:46:49 +0800

    publish1.0

---
+++ Dessert/DynamicData/FieldTemplates/step.ascx
@@ -0,0 +1,8 @@
+﻿<%@ Control Language="C#" CodeBehind="step.ascx.cs" Inherits="Dessert.DynamicData.FieldTemplates.step1" %>
+
+<asp:Literal runat="server" ID="Literal1" Text="<%# FieldValueString %>" />
+<br />
+<asp:TextBox ID="TextBox1" runat="server" Height="300px" TextMode="MultiLine" Width="720px" BorderColor="Black" BorderStyle="Solid" BorderWidth="1px"></asp:TextBox>
+<br />
+上传步骤配图：<asp:FileUpload ID="FileUpload1" runat="server" />
+
--- Dessert/Front/publishCourse.aspx.cs
+++ Dessert/Front/publishCourse.aspx.cs
@@ -9,9 +9,40 @@ namespace Dessert.Front
 {
     public partial class WebForm8 : System.Web.UI.Page
     {
+        int Count
+        {
+            //记录步骤数
+            get
+            {
+                if (ViewState["ControlCount"] == null)
+                    ViewState["ControlCount"] = 1;
+                return (int)ViewState["ControlCount"];
+            }
+            set
+            {
+                ViewState["ControlCount"] = value;
+            }
+        }
+        void LoadStep(int index)
+        {
+            Control ctl = this.LoadControl("../DynamicData/FieldTemplates/step.ascx");
+            ctl.ID = string.Format("setpControl_{0}", index);
+            this.PlaceHolder1.Controls.Add(ctl);
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //为增加步骤button添加click事件
+            Button1.Click += new EventHandler(Button1_Click);
+            for(int i = 1; i <= Count; i++)
+            {
+                LoadStep(i);
+            }
+            
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Count++;
+            LoadStep(Count);
         }
     }
 }
